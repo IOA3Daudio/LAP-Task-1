379c2eb9e837f8d5d7c700f1b6601db98f481dd9
IOA3Daudio <163104952+IOA3Daudio@users.noreply.github.com>
2025-01-12 16:00:42 +0800

    Update get_onset.m

--- matlab scripts/get_onset.m
+++ matlab scripts/get_onset.m
@@ -1,4 +1,5 @@
 function onset_sample = get_onset(HRIR)
+% Onsets obtained here are only used to guide the model training. They are not the harmonized ITDs and not used to reconstruct the final binaural HRIRs.
 onset_sample = AKonsetDetect(HRIR, 10, -20, 'rel', [3000 44100]);
 onset_sample = floor(onset_sample);
 end
